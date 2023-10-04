ab35aa38f22b46cd459d9b668518c165d78ba2b6
ambroisegithub <muhayimana21@gmail.com>
2023-10-04 13:07:28 +0200

    javascript maximum and minimum number in array

---
+++ maxAndmin.js
@@ -0,0 +1,24 @@
+const maximum = (array) => {
+
+    let max = array[0];
+
+    for (let index = 1; index < array.length; index++) {
+        if (array[index] > max)
+            max = array[index];
+    }
+    return max;
+
+}
+
+const minimum = (array) => {
+    let min = array[0]
+    for (let index = 1; index < array.length; index++) {
+        if (array[index] < min)
+            min = array[index]
+    }
+    return min;
+}
+
+let array = [10, 2, 30, 40, 50, 60, 109];
+console.log("The maximum number is:", maximum(array));
+console.log("The mimimum number is:", minimum(array));
