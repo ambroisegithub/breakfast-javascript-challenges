e40d12042b2cc0ffc817c4f262b8cfba0c3bc787
ambroisegithub <muhayimana21@gmail.com>
2023-10-07 19:11:00 +0200

    sum Of objects elements

---
+++ objects.js
@@ -0,0 +1,17 @@
+const Objects1 = {
+    students: [
+        "ambroise", "kenny", "belhla"
+    ],
+    professionals: {
+        names: {
+            ambroise: ["web developments"],
+            kenny: ["mathematics"],
+            belhla: "literature"
+        }
+
+
+    }
+}
+console.log(Objects1.students);
+console.log(Objects1.professionals);
+console.log(Objects1.professionals.names.ambroise);
