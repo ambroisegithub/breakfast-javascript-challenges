a76b7b7bc4e0608ffa9442f0dc35d4a279c2db80
ambroisegithub <muhayimana21@gmail.com>
2023-11-09 22:11:02 +0200

    reverse String

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,14 @@
 const reverseString = (str) => {
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+};
+
+console.log(reverseString("ambroise"));
+function reversestring(str) {
+  return str.split("").reverse().join("");
 }
 
-console.log(reverseString("isi"));
+console.log(reversestring("Hello, World!")); // Outputs: "!dlroW ,olleH"
