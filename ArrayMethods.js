0e89d62ed5bf4eb142100620acfa592e7a7984c7
ambroisegithub <muhayimana21@gmail.com>
2023-11-10 16:02:57 +0200

    array Methods

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -40,5 +40,68 @@ countries.pop();
 console.log(countries);
 
 //splice() methods: the general purpose of this is to change the contents of an array by removing,adding, replacing the elements of an array at specific index.;
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
 
-console.log(countries.splice(0, 4, "Congo"));
+//shift() It used to remove the first element in an array;
+fruits.shift();
+console.log(fruits);
+// unshift() Methods is used to add the element in array at the first index;
+fruits.unshift("Lemon");
+console.log(fruits);
+
+//join() methods: it is used to join the array elements as single string or number
+const elements = ["Fire", "Air", "Water"];
+
+console.log(elements.join());
+// expected output: "Fire,Air,Water"
+
+console.log(elements.join(""));
+// expected output: "FireAirWater"
+
+console.log(elements.join("-"));
+// filter() methods is used to create a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+var results = words.filter((word) => word.length > 8);
+console.log(results);
+
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+const beasts = ["ant", "bison", "camel", "duck", "bison"];
+
+console.log(beasts.indexOf("bison"));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf("bison", 2));
+// expected output: 4
+
+console.log(beasts.indexOf("giraffe"));
+// reverse()
+const reversearr = ["one", "two", "three"];
+console.log("array1:", reversearr);
+// expected output: "array1:" Array ["one", "two", "three"]
+
+const reversed = reversearr.reverse();
+console.log("reversed:", reversed);
+// expected output: "reversed:" Array ["three", "two", "one"]
+
+//sort
+
+// The sort() method sorts the elements of an array in place and returns the reference to the same array, now sorted. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
+// array.sort( compareFunction );
+const months = ["March", "Jan", "Feb", "Dec"];
+months.sort();
+console.log(months);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const arraysort = [1, 30, 4, 21, 100000];
+arraysort.sort();
+console.log(arraysort);
